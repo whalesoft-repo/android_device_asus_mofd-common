1f3de4470ae91320ef909cb1908d45b90c8d287e
nutcasev15 <tab.andsupp@gmail.com>
2017-06-05 10:40:18 +0530

    [INIT] Go Easy On The Juice
    Change-Id: Iba9844a4c8d09ee6a1ae2ea077d233c12d30d42a

--- init/init_mofd.cpp
+++ init/init_mofd.cpp
@@ -161,12 +161,12 @@ void set_feq_values()
 
     if(read_file2(MAX_CPU_FREQ, buf, sizeof(buf))) {
 	if ( strncmp(buf, LOW_CPU, strlen(LOW_CPU)) == 0 ) {
-            property_set("ro.sys.perf.device.powersave", "1250000");
-            property_set("ro.sys.perf.device.touchboost", "500000");
+            property_set("ro.sys.perf.device.powersave", "500000");
+            property_set("ro.sys.perf.device.touchboost", "666000");
             property_set("ro.sys.perf.device.full", "1833000");
         } else if ( strncmp(buf, HIGH_CPU, strlen(HIGH_CPU)) == 0 ) {
-            property_set("ro.sys.perf.device.powersave", "1500000");
-            property_set("ro.sys.perf.device.touchboost", "1833000");
+            property_set("ro.sys.perf.device.powersave", "500000");
+            property_set("ro.sys.perf.device.touchboost", "666000");
             property_set("ro.sys.perf.device.full", "2333000");
         } else {
             INFO("%s: Failed to get max cpu speed: %s\n", __func__, buf);

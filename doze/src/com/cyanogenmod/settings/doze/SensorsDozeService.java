c4aa2e0b3bf4f4deab1257fe90d1d2e28f6c07a7
nutcasev15 <tab.andsupp@gmail.com>
2017-08-14 12:23:36 +0530

    [DOZE] Reconfigure Timers
    Change-Id: Ia03dc27986cf7600a5c635803b3802331f0f63a1

--- doze/src/com/cyanogenmod/settings/doze/SensorsDozeService.java
+++ doze/src/com/cyanogenmod/settings/doze/SensorsDozeService.java
@@ -45,9 +45,9 @@ public class SensorsDozeService extends Service {
     private static final String DOZE_INTENT = "com.android.systemui.doze.pulse";
 
     private static final int HANDWAVE_DELTA_NS = 1000 * 1000 * 1000;
-    private static final int PULSE_MIN_INTERVAL_MS = 5000;
-    private static final int SENSORS_WAKELOCK_DURATION = 1000;
-    private static final int VIBRATOR_ACKNOWLEDGE = 40;
+    private static final int PULSE_MIN_INTERVAL_MS = 150000;
+    private static final int SENSORS_WAKELOCK_DURATION = 850;
+    private static final int VIBRATOR_ACKNOWLEDGE = 30;
 
     private static final String KEY_GESTURE_HAND_WAVE = "gesture_hand_wave";
     private static final String KEY_GESTURE_PICK_UP = "gesture_pick_up";

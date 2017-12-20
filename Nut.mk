2566d23a10c9cc55e96b658c1955a1230e4b2946
nutcasev15 <tab.andsupp@gmail.com>
2017-12-20 14:27:38 +0530

    Add Silvermont Specific TC Flags
    Change-Id: Idc76bc361dd82b29c673464ae84d0b6a6c24f762

--- Nut.mk
+++ Nut.mk
@@ -4,3 +4,16 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.substratum.verified=true
 
+# Toolchain Flags
+SILVERMONT_FLAGS := -march=silvermont \
+                    -mtune=silvermont \
+                    -msse4.2 \
+                    -mpopcnt \
+                    -fgcse-las \
+                    -mstackrealign \
+                    -Ofast -funsafe-math-optimizations
+
+BOARD_GLOBAL_CFLAGS += $(SILVERMONT_FLAGS)
+BOARD_GLOBAL_CPPFLAGS := $(SILVERMONT_FLAGS)
+BOARD_RELEASE_CFLAGS := $(SILVERMONT_FLAGS)
+

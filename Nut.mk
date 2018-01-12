723bc30a8bd37da73621220857b9dc7e574b17a7
nutcasev15 <tab.andsupp@gmail.com>
2018-01-12 17:58:06 +0530

    Pass Flags to CLANG Too
    Change-Id: If9d060cfb40a5b4da09e3c1da82678f4ab3796e9

--- Nut.mk
+++ Nut.mk
@@ -5,15 +5,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.substratum.verified=true
 
 # Toolchain Flags
-SILVERMONT_FLAGS := -march=silvermont \
-                    -mtune=silvermont \
+SILVERMONT_FLAGS := -march=slm \
+                    -mtune=slm \
                     -msse4.2 \
                     -mpopcnt \
                     -fgcse-las \
-                    -mstackrealign \
-                    -Ofast -funsafe-math-optimizations
+                    -mstackrealign
 
-BOARD_GLOBAL_CFLAGS += $(SILVERMONT_FLAGS)
-BOARD_GLOBAL_CPPFLAGS := $(SILVERMONT_FLAGS)
-BOARD_RELEASE_CFLAGS := $(SILVERMONT_FLAGS)
+BOARD_GLOBAL_CFLAGS += $(SILVERMONT_FLAGS) -Ofast -funsafe-math-optimizations
+BOARD_GLOBAL_CPPFLAGS := $(SILVERMONT_FLAGS) -Ofast -funsafe-math-optimizations
+BOARD_RELEASE_CFLAGS := $(SILVERMONT_FLAGS) -Ofast -funsafe-math-optimizations
+CLANG_CONFIG_EXTRA_CFLAGS += $(SILVERMONT_FLAGS) -O3 -funsafe-math-optimizations
+CLANG_CONFIG_EXTRA_CONLYFLAGS += $(SILVERMONT_FLAGS) -O3 -funsafe-math-optimizations
+CLANG_CONFIG_EXTRA_CPPFLAGS += $(SILVERMONT_FLAGS) -O3 -funsafe-math-optimizations
 
